a4746650e64f7a94651d3ddfb5f4ed2a4882712f
niceholholl <165501701+niceholholl@users.noreply.github.com>
2025-12-05 02:22:46 +0900

    Update degree_utils.py

--- network_tool_pkg/utils/degree_utils.py
+++ network_tool_pkg/utils/degree_utils.py
@@ -52,15 +52,15 @@ def preprocess_stub(degree_sequence) :
     raise TypeError('입력한 degree sequence의 형태가 올바르지 않습니다. list 형태로 입력하십시오.')
 
   if len(degree_sequence) == 0 :
-    raise ValueError('입력한 degree sequence는 빈 list 입니다. preprocessing.preprocess_network()로 전처리를 먼저 실행하십시오.')
+    raise ValueError('입력한 degree sequence는 빈 list 입니다. network_pkg.utils.degree_utils.create_degree_sequence()로 전처리를 먼저 실행하십시오.')
 
   for degree in degree_sequence :
 
     if not isinstance(degree, int) :
-      raise TypeError('입력한 degree sequence에는 정수 형태만 포함되어야합니다. 올바른 네트워크를 preprocessing.preprocess_network()로 전처리하십시오.')
+      raise TypeError('입력한 degree sequence에는 정수 형태만 포함되어야합니다. 올바른 네트워크를 network_pkg.utils.preprocessing.preprocess_network()로 전처리하십시오.')
     
     if degree < 0 :
-      raise ValueError('입력한 degree sequence에 음수 degree가 포함되어 있습니다. 올바른 네트워크를 preprocessing.preprocess_network()로 전처리하십시오.')
+      raise ValueError('입력한 degree sequence에 음수 degree가 포함되어 있습니다. 올바른 네트워크를 network_pkg.utils.preprocessing.preprocess_network()로 전처리하십시오.')
     
   # ---------- stub 총합이 짝수인 경우  ----------
 
@@ -83,7 +83,7 @@ def preprocess_stub(degree_sequence) :
     print('[stub prepocessing] 보정 완료 : 최소 차수({})를 1 증가시켜 {}로 수정하였습니다. 보정된 차수 합 : {}'.format(min_degree, min_degree+1, sum(degree_sequence)))
 
   except ValueError :
-    raise ValueError('입력한 degree sequence가 잘못된 형태입니다. 올바른 네트워크를 preprocessing.preprocess_network()로 전처리하십시오.')
+    raise ValueError('입력한 degree sequence가 잘못된 형태입니다. 올바른 네트워크를 network_pkg.utils.preprocessing.preprocess_network()로 전처리하십시오.')
 
   return degree_sequence
 
