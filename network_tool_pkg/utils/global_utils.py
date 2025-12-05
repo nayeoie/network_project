a82a8287065e59dc698ea780ab65307ae47b86fc
niceholholl <165501701+niceholholl@users.noreply.github.com>
2025-12-06 03:32:32 +0900

    Update global_utils.py

--- network_tool_pkg/utils/global_utils.py
+++ network_tool_pkg/utils/global_utils.py
@@ -9,15 +9,23 @@ def calculate_global(G) :
     raise TypeError('입력한 네트워크의 형태가 올바르지 않습니다. networkx.Graph 형태로 입력하십시오.')
 
   if len(G.nodes()) <= 1 :
-    raise ValueError('전역 지표 계산이 불가합니다. 노드 수가 1 이하입니다.')
+    raise ValueError('전역 지표 계산이 불가능합니다. 노드 수가 1 이하입니다.')
 
   if len(G.edges()) == 0 :
-    raise ValueError('전역 지표 계산이 불가합니다. 엣지가 존재하지 않습니다.')
+    raise ValueError('전역 지표 계산이 불가능합니다. 엣지가 존재하지 않습니다.')
 
-  # ---------- 클러스터링 계수 ----------
-  
+  if not nx.is_connected(G) :
+    raise ValueError('입력한 네트워크의 형태가 connected graph가 아닙니다. 평균 경로 길이 및 지름 계산이 불가능합니다.')
+
+  # ---------- 전역 지표 생성 ----------
+
+  # 클러스터링 계수
   cc = nx.average_clustering(G)
 
-  # ---------- 평균 경로 길이, 지름 ----------
+  # 평균 경로 길이
+  apl = nx.average_shortest_path_length(G)
 
-  #
+  # 지름
+  diam = nx.diameter(G)
+
+  return {'CC' : cc, 'APL' : apl, 'DIAM' : diam}
